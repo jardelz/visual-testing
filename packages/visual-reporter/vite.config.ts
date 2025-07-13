0eb61855997686cd379500820299c6e270284380
Jardel <jazanolla@gmail.com>
2025-07-13 20:05:10 -0300

    Test

--- packages/visual-reporter/vite.config.ts
+++ packages/visual-reporter/vite.config.ts
@@ -1,26 +1,29 @@
-import { vitePlugin as remix } from '@remix-run/dev'
 import { defineConfig } from 'vite'
+import { vitePlugin as remix } from '@remix-run/dev'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
-const baseName = './'
+const baseName = process.env.GITHUB_PAGES || ''
 
 export default defineConfig({
-    plugins: [
-        remix({
-            ...(baseName && { basename: baseName }),
-            future: {
-                v3_fetcherPersist: true,
-                v3_relativeSplatPath: true,
-                v3_throwAbortReason: true,
-            },
-            ssr: false,
-        }),
-        tsconfigPaths(),
-    ],
-    ...(baseName && { base: baseName }),
-    css: {
-        modules: {
-            localsConvention: 'camelCaseOnly',
-        },
+  plugins: [
+    remix({
+      ...(baseName && { basename: baseName }),
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+      ssr: false,
+    }),
+    tsconfigPaths(),
+    viteSingleFile(), // <-- plugin para embutir tudo
+  ],
+  ...(baseName && { base: baseName }),
+  base: './', // <-- importante para funcionar em file://
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
     },
+  },
 })
